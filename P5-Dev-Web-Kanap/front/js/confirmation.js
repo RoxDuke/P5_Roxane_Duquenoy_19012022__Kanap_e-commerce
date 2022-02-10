ee0e9ddc32dce678823cfff9b468a5bb1765a875
RoxDuke <duquenoyroxane11@gmail.com>
2022-02-10 17:02:07 +0100

    Envoi au serveur, confirmation de commande

--- P5-Dev-Web-Kanap/front/js/confirmation.js
+++ P5-Dev-Web-Kanap/front/js/confirmation.js
@@ -1,13 +1,36 @@
-//Récupération du panier et du contact 
+//Récupération du panier et du contact pour confirmer la commande
 let basket = getBasket();
 let contact = getContact();
+let products = basket.map(function(productId){
+    return productId
+})
 
-//Requête POST vers l'API 
-// fetch(//URL ,{
-//     method: "POST"
-//     headers: {
-//         'Accept':'application/json',
-//         'Content-Type':'application/json'
-//     }
-//     body:JSON.stringify(jsonBODY)
-// })
+let orderConfirm = {
+    contact : contact,
+    products : products,
+    
+}
+console.log(orderConfirm)
+// Requête POST vers l'API pour envoyer la commande
+//Paramètres pour utiliser la requête POST
+let sendParams = {
+    method: "POST",
+    headers: {
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+    },
+    body: JSON.stringify(orderConfirm)
+};
+//Envoi vers le serveur 
+fetch("http://localhost:3000/api/products/order", sendParams)
+    .then(function(order){
+        let orderId = document.getElementById("orderId");
+        let newOrderId = document.createTextNode(`${order.orderId}`);
+        if(newOrderId != undefined){
+            orderId.replaceChild(newOrderId, orderId.childNodes[0]);
+        }
+    
+})
+    
+
+localStorage.clear()
